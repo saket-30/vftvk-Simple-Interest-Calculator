035be4301e5c718ad6978f6c675c779c98214ea2
saket-30 <81294010+saket-30@users.noreply.github.com>
2021-12-12 00:27:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function Calculate()
+    {
+        let p=document.getElementById('amount').value;
+        let t=document.getElementById('time').value;
+        let r=document.getElementById('rate').value;
+
+        let  SI=(p*t*r)/100;
+
+        document.getElementById('si').innerHTML="The Total Simple Intrest Is: "+SI;
+    }
